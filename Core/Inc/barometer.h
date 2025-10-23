ea6cd8c6d06a7293c89094ba124d189debab7348
Rylan Meilutis <rylan.meilutis@gmail.com>
2025-10-23 16:38:54 -0400

    ncrease the range of acceptable values to consider to be 0

--- Core/Inc/barometer.h
+++ Core/Inc/barometer.h
@@ -121,7 +121,7 @@
 #define BMP390_HYPSOMETRIC_EXPONENT        0.1903f
 
 // Tunables to prevent drift
-#define ZERO_HYST_M      0.25f   // +/- band around 0 m to avoid chattering
+#define ZERO_HYST_M      0.5f   // +/- band around 0 m to avoid chattering
 #define RESET_STEP_M     5.0f    // recalibrate every ~5 m from the last baseline
 
 // ==============================
