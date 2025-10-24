2986dbc79aee099d229b8e392a9ba5316ee16219
Rylan Meilutis <rylan.meilutis@gmail.com>
2025-10-23 23:50:34 -0400

    support the current split Gyroscope / Accelerometer setup

--- sedsprintf_rs/src/config.rs
+++ sedsprintf_rs/src/config.rs
@@ -47,23 +47,25 @@ impl DataEndpoint {
 pub enum DataType {
     TelemetryError = 0,
     GpsData = 1,
-    ImuData = 2,
-    BatteryStatus = 3,
-    SystemStatus = 4,
-    BarometerData = 5,
+    GyroscopeData = 2,
+    AccelerometerData = 3,
+    BatteryStatus = 4,
+    SystemStatus = 5,
+    BarometerData = 6,
 }
 
 pub const MAX_VALUE_DATA_TYPE: u32 = DataType::BarometerData as u32;
 
 impl DataType {
-    pub const COUNT: usize = 6;
+    pub const COUNT: usize = 7;
 
     #[inline]
     pub fn as_str(self) -> &'static str {
         match self {
             DataType::TelemetryError => "TELEMETRY_ERROR",
             DataType::GpsData => "GPS_DATA",
-            DataType::ImuData => "IMU_DATA",
+            DataType::GyroscopeData => "Gyroscope_Data",
+            DataType::AccelerometerData => "ACCELEROMETER_DATA",
             DataType::BatteryStatus => "BATTERY_STATUS",
             DataType::SystemStatus => "SYSTEM_STATUS",
             DataType::BarometerData => "BAROMETER_DATA",
@@ -76,12 +78,14 @@ pub const MESSAGE_DATA_TYPES: [MessageDataType; DataType::COUNT] = [
     MessageDataType::Float32,
     MessageDataType::Float32,
     MessageDataType::Float32,
+    MessageDataType::Float32,
     MessageDataType::UInt8,
     MessageDataType::Float32,
 ];
 
 pub const MESSAGE_INFO_TYPES: [MessageType; DataType::COUNT] = [
     MessageType::Error,
+    MessageType::Info,
     MessageType::Info,
     MessageType::Info,
     MessageType::Info,
@@ -103,7 +107,8 @@ pub const fn data_type_size(dt: MessageDataType) -> usize {
 pub const MESSAGE_ELEMENTS: [usize; DataType::COUNT] = [
     1, // elements int he Telemetry Error data
     3, // elements in the GPS data (lat, lon, alt)
-    6, // elements in the IMU data (accel x,y,z; gyro x,y,z; mag x,y,z)
+    3, // elements in the Gyroscope data (gyro x,y,z)
+    3, // elements in the Accelerometer data (accel x,y,z)
     4, // elements in the Battery Status data
     2, // elements in the System Status data (cpu load, memory usage)
     3, // elements in the Barometer data (pressure, temperature, altitude)
@@ -123,7 +128,11 @@ pub const MESSAGE_TYPES: [MessageMeta; DataType::COUNT] = [
         endpoints: &[DataEndpoint::SdCard, DataEndpoint::Radio],
     },
     MessageMeta {
-        data_size: get_needed_message_size(DataType::ImuData),
+        data_size: get_needed_message_size(DataType::GyroscopeData),
+        endpoints: &[DataEndpoint::SdCard, DataEndpoint::Radio],
+    },
+    MessageMeta {
+        data_size: get_needed_message_size(DataType::AccelerometerData),
         endpoints: &[DataEndpoint::SdCard, DataEndpoint::Radio],
     },
     MessageMeta {
