d4d6ecd2a0c35a642a480a678558dbd112480e08
Rylan Meilutis <rylan.meilutis@gmail.com>
2025-10-28 21:35:55 -0400

    removed unneeded file

--- Core/Src/freertos_alloc_shim.c
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-#include <stdint.h>
-#include <stdlib.h>
-
-// Minimal shims so code that calls pvPortMalloc/vPortFree links without
-// FreeRTOS.
-void *pvPortMalloc(size_t xSize) { return malloc(xSize); }
-
-void vPortFree(void *pv) { free(pv); }
