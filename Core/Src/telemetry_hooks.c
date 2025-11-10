bcb92d6f5f44fe847925ae52738e1158c28b9c4c
Rylan Meilutis <rylan.meilutis@gmail.com>
2025-11-09 20:06:52 -0500

    telemetry library version 1.1

--- Core/Src/telemetry_hooks.c
+++ Core/Src/telemetry_hooks.c
@@ -2,12 +2,14 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "FreeRTOS.h"
+#include <stdio.h>
 
 /*
  * Rust expects these functions to exist for heap allocations:
  *
  *   void *telemetryMalloc(size_t);
  *   void telemetryFree(void *);
+ *   void seds_println(const char *, size_t);
  *
  */
 
@@ -20,4 +22,11 @@ void *telemetryMalloc(size_t xSize)
 void telemetryFree(void *pv)
 {
     vPortFree(pv);
+}
+
+void seds_println(const char *str, size_t len)
+{
+    (void)len;
+    /* Simple implementation using standard printf */
+    printf("%s\r\n", str);
 }
